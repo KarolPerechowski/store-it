f57bc15882afd4db114573249f67040c924f5718
KarolPerechowski <karol.perechowski@gmail.com>
2025-04-02 01:08:03 +0200

    fixed some issues, made first prisma migration

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import { PrismaClient } from "./generated/prisma"
 
 const app = express();
+const prisma = new PrismaClient();
 
 const port = process.env.PORT || 3000;
 
@@ -11,3 +13,8 @@ app.get("/", (req: express.Request, res: express.Response) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
+/*
+app.get('/users', (req: express.Request, res: express.Response) => {
+  const users = prisma.user.findMany();
+  res.json(users);
+})*/
